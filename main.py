5f54816fd369716770b8e52d595a82932a5f7b7c
Katie Germano <kt.germano@gmail.com>
2017-09-18 10:28:45 -0500

    Added code to process form and casesar code

---
+++ main.py
@@ -0,0 +1,54 @@
+from flask import Flask, request
+from caesar import rotate_string
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+
+form = """
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+      <!-- create your form here -->
+      <form action="/" method="post">
+      <label for="rot">Rotate by:</label>
+      <input type="text" name="rot" value="0" />
+      <input type="text-area" name="text"/>
+      <input type="submit" />
+    </body>
+</html>
+"""
+
+
+@app.route("/")
+def index():
+    return form
+
+@app.route("/", methods= ['POST'])
+def encrypt():
+    text= request.form['text']
+    rot= request.form['rot']
+    encrypted_string = rotate_string(text, rot)
+    
+    return '<h1>' + str(encrypted_string) + '</h1>'
+
+
+app.run()
